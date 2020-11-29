1779ce9e4fe5d05cc155c1c8e1050089b8b8880c
anatoliy <doberman24@yandex.ru>
2020-11-29 19:43:43 +0300

    Test commit 1

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -1,5 +1,5 @@
 <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-  <h5 class="my-0 mr-md-auto font-weight-normal">Проект Laravel</h5>
+  <h5 class="my-0 mr-md-auto font-weight-normal">Проект Laravel в Выборге</h5>
   <nav class="my-2 my-md-0 mr-md-3">
     <a class="p-2 text-dark" href="{{ route('home') }}">Главная</a>
     <a class="p-2 text-dark" href="{{ route('about') }}">Про нас</a>
